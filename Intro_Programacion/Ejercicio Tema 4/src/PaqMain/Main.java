36641465436e708cc38c6b54ca1ab024946effd4
Gabriel Gonzalez Carvajal <gabgc.z930@gmail.com>
2022-09-20 05:39:37 +0200

    Ejercicios tema4

---
+++ Intro_Programacion/Ejercicio Tema 4/src/PaqMain/Main.java
@@ -0,0 +1,11 @@
+package PaqMain;
+
+public class Main {
+    public static void main (String[] args) {
+        int contador = 2;
+        System.out.println(contador);
+        contador = contador - 1;
+
+        while (contador > 2){}
+    }
+}

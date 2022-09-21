3e012c612600f3225c50a89b5567d448e46def3d
Gabriel Gonzalez Carvajal <gabgc.z930@gmail.com>
2022-09-21 04:39:59 +0200

    fin Ej tm4

--- Intro_Programacion/Ejercicio Tema 4/src/PaqMain/Main.java
+++ Intro_Programacion/Ejercicio Tema 4/src/PaqMain/Main.java
@@ -2,10 +2,53 @@ package PaqMain;
 
 public class Main {
     public static void main (String[] args) {
-        int contador = 2;
-        System.out.println(contador);
-        contador = contador - 1;
 
-        while (contador > 2){}
+        // Bucle if else
+        int numeroIf = -1;
+        if (numeroIf > 0){
+            System.out.println("Número positivo");
+        } else if (numeroIf < 0){
+            System.out.println("Número negativo");
+        } else {
+            System.out.println("Número cero");
+        }
+
+       //Bucle While
+        int numeroWhile = 0;
+        while (numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+
+        //Bucle Do while
+        do {
+            System.out.println(numeroWhile);
+            numeroWhile = numeroWhile -1;
+        } while (numeroWhile < 1);
+
+        //Bucle for
+        for (int numeroFor = 0; numeroFor <=3; numeroFor++)
+            System.out.println(numeroFor);
+
+        //Bucle Switch case
+        var estacion = "Otoño";
+
+        switch (estacion){
+            case "Primavera":
+                System.out.println("Es Primavera");
+                break;
+            case "Verano":
+                System.out.println("Es Verano");
+                break;
+            case "Otoño":
+                System.out.println("Es Otoño");
+                break;
+            case "Invierno":
+                System.out.println("Es Invierno");
+                break;
+            default:
+                System.out.println("No es una estación");
+                break;
+        }
     }
 }
